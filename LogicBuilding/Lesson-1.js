cd799f9546524cc996fdca26d16d9e70577dc290
DevilDeveloper <iamdevildeveloper@gmail.com>
2025-07-08 10:34:17 +0530

    Lesson 1 Problem solved

---
+++ LogicBuilding/Lesson-1.js
@@ -0,0 +1,15 @@
+// Print Hello World to the console
+// console.log('Welcome to Logic Building Lesson 1!');
+
+
+// Write a function that takes two numbers and returns their sum
+// function sum(a, b) {
+//     return a + b;
+// }
+// console.log(sum(5, 10));
+
+// Write a function that calculates and prints the area of rectangle given its length and width
+// function areaOfRectangle(length, width) {
+//     return length * width
+// }
+// console.log(areaOfRectangle(5, 10));
